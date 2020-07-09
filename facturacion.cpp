393f7f9c17186c60e8d6e287fb9cef27d64e75bb
jeisonh0423 <jeisonescobar000@gmail.com>
2020-07-09 10:31:10 +0200

    commit inicial

---
+++ facturacion.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int factura, codigoproducto, cantidaddetalles=0;
+    double precio, cantidad, subtotaldetalle=0, subtotal=0, descuento=0, isv=0, totalpagar=0;
+    string cliente, producto;
+    const double PORCENTAJEDESCUENTO = 0.05;
+    const double PORCENTAJEISV = 0.015;
+    cout << "Introduzca la factura: ";
+    cin >> factura;
+    cout << "Introduzca el cliente: ";
+    cin >> cliente;
+    cout << "cuantos productos lleva?: ";
+    cin >> cantidaddetalles;
+    for (int i = 0; i < cantidaddetalles; i++)
+    {
+        cout << "Introduzca el codigo producto: ";
+        cin >> codigoproducto;
+        cout << "Introduzca el producto: ";
+        cin >> producto;
+        cout << "Introduzca la cantidad: ";
+        cin >> cantidad;
+        cout << "Introduzca el precio: ";
+        cin >> precio;
+        subtotaldetalle=(cantidad*precio);
+        cout << "El Subtotal del detalle es: " <<subtotaldetalle << endl;
+        subtotal+=subtotaldetalle;
+    }
+    descuento=subtotal * PORCENTAJEDESCUENTO;
+    isv = subtotal * PORCENTAJEISV;
+    totalpagar = subtotal - descuento + isv;
+    cout << "El subtotal es: "<< subtotal << endl;
+    cout << "El descuento es: "<< descuento << endl;
+    cout << "El ISV es: "<< isv << endl;
+    cout << "El total a pagar es: " << totalpagar << endl;
+
+    return 0;
+}
